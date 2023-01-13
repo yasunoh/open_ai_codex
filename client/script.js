eee0daa7423bcfab53132bd67598bd45c293b8aa
tekku.taro <tekku.taro@gmail.com>
2023-01-13 13:16:11 +0900

    update server url

--- client/script.js
+++ client/script.js
@@ -1,6 +1,7 @@
 import bot from "./assets/bot.svg"
 import user from "./assets/user.svg"
 
+const serverUrl = "https://codex-lnkw.onrender.com"
 const form = document.querySelector('form');
 const chatContainer = document.querySelector('#chat_container')
 
@@ -79,7 +80,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server -> bot's response
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch(serverUrl, {
     method: 'POST',
     headers: {
       'Content-Type' : 'application/json'
